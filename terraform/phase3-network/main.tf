5124d304e6f67b27b1109c4b682e5be6049377bb
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-08-16 15:28:35 +0530

    all terraform code

--- terraform/phase3-network/main.tf
+++ terraform/phase3-network/main.tf
@@ -1,26 +1,64 @@
 # ============================================================
 # PHASE 3
-# TRANSIT GATEWAY + RAM + HUB/SPOKE ATTACHMENTS
+# TRANSIT GATEWAY + RAM + CROSS ACCOUNT ATTACHMENTS
 # ============================================================
 
 
 # ============================================================
-# TRANSIT GATEWAY MODULE
+# 1. CREATE TRANSIT GATEWAY
+# HUB ACCOUNT: 647132523867
 # ============================================================
 
-module "transit_gateway" {
-  source = "../modules/transit-gateway"
+resource "aws_ec2_transit_gateway" "hub_spoke" {
+  provider = aws
 
-  name = "hub-spoke-tgw"
+  description = "Hub Spoke Transit Gateway"
 
   amazon_side_asn = 64512
 
-  spoke_account_id = "434097521299"
+  default_route_table_association = "disable"
+  default_route_table_propagation  = "disable"
+
+  dns_support = "enable"
+
+  tags = {
+    Name = "hub-spoke-tgw"
+  }
 }
 
 
 # ============================================================
-# HUB VPC
+# 2. CREATE HUB TGW ROUTE TABLE
+# ============================================================
+
+resource "aws_ec2_transit_gateway_route_table" "hub" {
+  provider = aws
+
+  transit_gateway_id = aws_ec2_transit_gateway.hub_spoke.id
+
+  tags = {
+    Name = "hub-tgw-route-table"
+  }
+}
+
+
+# ============================================================
+# 3. CREATE SPOKE TGW ROUTE TABLE
+# ============================================================
+
+resource "aws_ec2_transit_gateway_route_table" "spoke" {
+  provider = aws
+
+  transit_gateway_id = aws_ec2_transit_gateway.hub_spoke.id
+
+  tags = {
+    Name = "spoke-tgw-route-table"
+  }
+}
+
+
+# ============================================================
+# 4. FIND HUB VPC
 # ACCOUNT: 647132523867
 # ============================================================
 
@@ -35,7 +73,7 @@ data "aws_vpc" "hub" {
 
 
 # ============================================================
-# HUB PRIVATE SUBNETS
+# 5. FIND HUB PRIVATE SUBNETS
 # ============================================================
 
 data "aws_subnets" "hub_private" {
@@ -54,7 +92,7 @@ data "aws_subnets" "hub_private" {
 
 
 # ============================================================
-# SPOKE VPC
+# 6. FIND SPOKE VPC
 # ACCOUNT: 434097521299
 # ============================================================
 
@@ -69,7 +107,7 @@ data "aws_vpc" "spoke" {
 
 
 # ============================================================
-# SPOKE PRIVATE SUBNETS
+# 7. FIND SPOKE PRIVATE SUBNETS
 # ============================================================
 
 data "aws_subnets" "spoke_private" {
@@ -88,7 +126,51 @@ data "aws_subnets" "spoke_private" {
 
 
 # ============================================================
-# HUB TGW ATTACHMENT
+# 8. RAM RESOURCE SHARE
+# HUB ACCOUNT
+# ============================================================
+
+resource "aws_ram_resource_share" "tgw" {
+  provider = aws
+
+  name = "hub-spoke-tgw-share"
+
+  allow_external_principals = true
+
+  tags = {
+    Name = "hub-spoke-tgw-share"
+  }
+}
+
+
+# ============================================================
+# 9. SHARE TGW THROUGH RAM
+# ============================================================
+
+resource "aws_ram_resource_association" "tgw" {
+  provider = aws
+
+  resource_share_arn = aws_ram_resource_share.tgw.arn
+
+  resource_arn = aws_ec2_transit_gateway.hub_spoke.arn
+}
+
+
+# ============================================================
+# 10. SHARE WITH SPOKE ACCOUNT
+# ============================================================
+
+resource "aws_ram_principal_association" "spoke" {
+  provider = aws
+
+  resource_share_arn = aws_ram_resource_share.tgw.arn
+
+  principal = "434097521299"
+}
+
+
+# ============================================================
+# 11. HUB VPC -> TGW ATTACHMENT
 # ============================================================
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "hub" {
@@ -98,7 +180,7 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "hub" {
 
   subnet_ids = data.aws_subnets.hub_private.ids
 
-  transit_gateway_id = module.transit_gateway.transit_gateway_id
+  transit_gateway_id = aws_ec2_transit_gateway.hub_spoke.id
 
   transit_gateway_default_route_table_association = false
   transit_gateway_default_route_table_propagation  = false
@@ -110,7 +192,7 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "hub" {
 
 
 # ============================================================
-# SPOKE TGW ATTACHMENT
+# 12. SPOKE VPC -> TGW ATTACHMENT
 # ============================================================
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "spoke" {
@@ -120,7 +202,7 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "spoke" {
 
   subnet_ids = data.aws_subnets.spoke_private.ids
 
-  transit_gateway_id = module.transit_gateway.transit_gateway_id
+  transit_gateway_id = aws_ec2_transit_gateway.hub_spoke.id
 
   transit_gateway_default_route_table_association = false
   transit_gateway_default_route_table_propagation  = false
@@ -132,48 +214,64 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "spoke" {
 
 
 # ============================================================
-# HUB ATTACHMENT -> HUB TGW ROUTE TABLE
+# 13. HUB ATTACHMENT -> HUB TGW ROUTE TABLE
 # ============================================================
 
 resource "aws_ec2_transit_gateway_route_table_association" "hub" {
-  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.hub.id
+  provider = aws
 
-  transit_gateway_route_table_id = module.transit_gateway.hub_route_table_id
+  transit_gateway_attachment_id =
+    aws_ec2_transit_gateway_vpc_attachment.hub.id
+
+  transit_gateway_route_table_id =
+    aws_ec2_transit_gateway_route_table.hub.id
 }
 
 
 # ============================================================
-# SPOKE ATTACHMENT -> SPOKE TGW ROUTE TABLE
+# 14. SPOKE ATTACHMENT -> SPOKE TGW ROUTE TABLE
 # ============================================================
 
 resource "aws_ec2_transit_gateway_route_table_association" "spoke" {
-  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.spoke.id
+  provider = aws
 
-  transit_gateway_route_table_id = module.transit_gateway.spoke_route_table_id
+  transit_gateway_attachment_id =
+    aws_ec2_transit_gateway_vpc_attachment.spoke.id
+
+  transit_gateway_route_table_id =
+    aws_ec2_transit_gateway_route_table.spoke.id
 }
 
 
 # ============================================================
-# HUB TGW -> SPOKE
+# 15. HUB TGW -> SPOKE
 # ============================================================
 
 resource "aws_ec2_transit_gateway_route" "hub_to_spoke" {
-  transit_gateway_route_table_id = module.transit_gateway.hub_route_table_id
+  provider = aws
+
+  transit_gateway_route_table_id =
+    aws_ec2_transit_gateway_route_table.hub.id
 
   destination_cidr_block = "10.1.0.0/16"
 
-  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.spoke.id
+  transit_gateway_attachment_id =
+    aws_ec2_transit_gateway_vpc_attachment.spoke.id
 }
 
 
 # ============================================================
-# SPOKE TGW -> HUB
+# 16. SPOKE TGW -> HUB
 # ============================================================
 
 resource "aws_ec2_transit_gateway_route" "spoke_to_hub" {
-  transit_gateway_route_table_id = module.transit_gateway.spoke_route_table_id
+  provider = aws
+
+  transit_gateway_route_table_id =
+    aws_ec2_transit_gateway_route_table.spoke.id
 
   destination_cidr_block = "10.0.0.0/16"
 
-  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.hub.id
+  transit_gateway_attachment_id =
+    aws_ec2_transit_gateway_vpc_attachment.hub.id
 }
