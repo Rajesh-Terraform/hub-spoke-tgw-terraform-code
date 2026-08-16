676fd84b26ca96cbf5958ee8fa4e0ccbe465456c
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-08-16 16:05:54 +0530

    all terraform code

--- terraform/hub/phase1-hubvpc/main.tf
+++ terraform/hub/phase1-hubvpc/main.tf
@@ -1,34 +1,28 @@
-module "hub_vpc" {
-  source = "../../modules/vpc"
-
-  vpc_name = "hub-vpc"
-
-  vpc_cidr = "10.0.0.0/16"
-
-  availability_zones = [
-    "ap-south-1a",
-    "ap-south-1b"
-  ]
-
-  public_subnet_cidrs = [
-    "10.0.0.0/24",
-    "10.0.1.0/24"
-  ]
-
-  private_subnet_cidrs = [
-    "10.0.10.0/24",
-    "10.0.11.0/24"
-  ]
-
-  create_igw          = true
-  create_public_subnets = true
-  create_nat_gateways = true
+data "aws_vpc" "hub" {
+  id = var.hub_vpc_id
 }
 
+data "aws_subnets" "hub_private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.hub.id]
+  }
+
+  filter {
+    name   = "tag:Tier"
+    values = ["private"]
+  }
+}
+
+module "transit_gateway" {
+  source = "./modules/transit-gateway"
+
+  name             = var.tgw_name
+  amazon_side_asn  = var.amazon_side_asn
+  spoke_account_id = var.spoke_account_id
+}
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "hub" {
-  provider = aws
-
   vpc_id = data.aws_vpc.hub.id
 
   subnet_ids = [
@@ -38,11 +32,7 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "hub" {
 
   transit_gateway_id = module.transit_gateway.transit_gateway_id
 
-  transit_gateway_default_route_table_association = false
-  transit_gateway_default_route_table_propagation  = false
-
   tags = {
-    Name = "hub-tgw-attachment"
+    Name = "${var.tgw_name}-hub-attachment"
   }
 }
-
--- terraform/modules/transit-gateway/outputs.tf
+++ terraform/modules/transit-gateway/outputs.tf
@@ -1,15 +1,4 @@
 output "transit_gateway_id" {
-  value = aws_ec2_transit_gateway.this.id
-}
-
-output "hub_route_table_id" {
-  value = aws_ec2_transit_gateway_route_table.hub.id
-}
-
-output "spoke_route_table_id" {
-  value = aws_ec2_transit_gateway_route_table.spoke.id
-}
-
-output "ram_share_arn" {
-  value = aws_ram_resource_share.tgw.arn
+  description = "Transit Gateway ID"
+  value       = aws_ec2_transit_gateway.this.id
 }
