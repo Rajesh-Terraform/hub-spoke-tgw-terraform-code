ef79b06648813f84f29d88cf7edd406c806d424c
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-08-17 17:58:46 +0530

    all terraform code changes

--- live/tgw/outputs.tf
+++ live/tgw/outputs.tf
@@ -9,15 +9,3 @@ output "hub_attachment_id" {
 output "spoke_attachment_id" {
   value = module.tgw.spoke_attachment_id
 }
-
-output "hub_tgw_route_table_id" {
-  value = module.tgw.hub_route_table_id
-}
-
-output "spoke_tgw_route_table_id" {
-  value = module.tgw.spoke_route_table_id
-}
-
-output "ram_share_arn" {
-  value = module.tgw.ram_share_arn
-}
