573c56e3bd7a72089ea8ca575540fb01d5b286e0
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-08-17 17:48:58 +0530

    all terraform code changes

--- modules/tgw/main.tf
+++ modules/tgw/main.tf
@@ -1,45 +1,10 @@
-data "aws_subnets" "hub_private" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.hub.id]
-  }
-
-  filter {
-    name   = "tag:Tier"
-    values = ["private"]
-  }
-}
-
-resource "aws_ec2_transit_gateway_vpc_attachment" "hub" {
-  provider = aws.hub
-
-  vpc_id = data.aws_vpc.hub.id
-
-  subnet_ids = [
-    data.aws_subnets.hub_private.ids[0],
-    data.aws_subnets.hub_private.ids[1]
-  ]
-
-  transit_gateway_id = aws_ec2_transit_gateway.this.id
-}
-
-# =========================================================
-# TRANSIT GATEWAY
-# =========================================================
-
-resource "aws_ec2_transit_gateway" "this" {
+resource "aws_ec2_transit_gateway" "main" {
   provider = aws.hub
 
   description = var.name
 
-  amazon_side_asn = 64512
-
-  auto_accept_shared_attachments = "disable"
-
   default_route_table_association = "disable"
   default_route_table_propagation = "disable"
-
-  dns_support = "enable"
 
   tags = merge(
     var.tags,
@@ -49,36 +14,40 @@ resource "aws_ec2_transit_gateway" "this" {
   )
 }
 
-# =========================================================
-# RAM SHARE
-# =========================================================
 
-resource "aws_ram_resource_share" "tgw" {
+# HUB VPC ATTACHMENT
+resource "aws_ec2_transit_gateway_vpc_attachment" "hub" {
   provider = aws.hub
 
-  name = "${var.name}-share"
-
-  allow_external_principals = true
+  transit_gateway_id = aws_ec2_transit_gateway.main.id
+  vpc_id             = var.hub_vpc_id
+  subnet_ids         = var.hub_subnet_ids
 
   tags = merge(
     var.tags,
     {
-      Name = "${var.name}-ram-share"
+      Name = "${var.name}-hub-attachment"
     }
   )
 }
 
-resource "aws_ram_resource_association" "tgw" {
-  provider = aws.hub
 
-  resource_share_arn = aws_ram_resource_share.tgw.arn
-  resource_arn       = aws_ec2_transit_gateway.this.arn
+# SPOKE VPC ATTACHMENT
+resource "aws_ec2_transit_gateway_vpc_attachment" "spoke" {
+  provider = aws.spoke
+
+  transit_gateway_id = aws_ec2_transit_gateway.main.id
+  vpc_id             = var.spoke_vpc_id
+  subnet_ids         = var.spoke_subnet_ids
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.name}-spoke-attachment"
+    }
+  )
+
+  depends_on = [
+    aws_ec2_transit_gateway_vpc_attachment.hub
+  ]
 }
-
-resource "aws_ram_principal_association" "spoke" {
-  provider = aws.hub
-
-  principal          = var.spoke_account_id
-  resource_share_arn = aws_ram_resource_share.tgw.arn
-}
-
--- modules/tgw/outputs.tf
+++ modules/tgw/outputs.tf
@@ -1,23 +1,22 @@
 output "transit_gateway_id" {
-  description = "Transit Gateway ID"
-  value       = aws_ec2_transit_gateway.main.id
+  value = aws_ec2_transit_gateway.main.id
 }
 
 output "hub_vpc_id" {
-  description = "Hub VPC ID"
-  value       = data.aws_vpc.hub.id
+  value = var.hub_vpc_id
 }
 
-output "hub_private_subnet_ids" {
-  description = "Hub private subnet IDs"
-  value       = data.aws_subnets.hub_private.ids
+output "spoke_vpc_id" {
+  value = var.spoke_vpc_id
 }
 
-output "hub_tgw_attachment_id" {
-  description = "Hub Transit Gateway attachment ID"
-  value       = aws_ec2_transit_gateway_vpc_attachment.hub.id
+output "hub_attachment_id" {
+  value = aws_ec2_transit_gateway_vpc_attachment.hub.id
 }
 
+output "spoke_attachment_id" {
+  value = aws_ec2_transit_gateway_vpc_attachment.spoke.id
+}
 
 
 
