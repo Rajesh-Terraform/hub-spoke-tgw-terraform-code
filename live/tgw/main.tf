f64afe64a48e8a81b8be00bbc1a51be8705799ed
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-08-17 17:12:50 +0530

    all terraform code changes

--- live/tgw/main.tf
+++ live/tgw/main.tf
@@ -1,3 +1,13 @@
+provider "aws" {
+  alias  = "hub"
+  region = var.region
+}
+
+provider "aws" {
+  alias  = "spoke"
+  region = var.region
+}
+
 data "terraform_remote_state" "hub" {
   backend = "s3"
 
@@ -16,47 +26,33 @@ data "terraform_remote_state" "spoke" {
     key    = "networking/spoke/terraform.tfstate"
     region = var.region
   }
-
-
-
-    providers = {
-    aws.hub   = aws.hub
-    aws.spoke = aws.spoke
-  }
-
 }
-
-
-
-
 
 module "tgw" {
   source = "../../modules/tgw"
+
+  providers = {
+    aws.hub   = aws.hub
+    aws.spoke = aws.spoke
+  }
 
   name = "hub-tgw"
 
   spoke_account_id = var.spoke_account_id
 
   hub_vpc_id = data.terraform_remote_state.hub.outputs.vpc_id
-
   hub_vpc_cidr = data.terraform_remote_state.hub.outputs.vpc_cidr
-
   hub_subnet_ids = data.terraform_remote_state.hub.outputs.private_subnet_ids
 
   hub_public_route_table_id = data.terraform_remote_state.hub.outputs.public_route_table_id
-
   hub_private_route_table_ids = data.terraform_remote_state.hub.outputs.private_route_table_ids
 
   spoke_vpc_id = data.terraform_remote_state.spoke.outputs.vpc_id
-
   spoke_vpc_cidr = data.terraform_remote_state.spoke.outputs.vpc_cidr
-
   spoke_subnet_ids = data.terraform_remote_state.spoke.outputs.private_subnet_ids
-
   spoke_route_table_ids = data.terraform_remote_state.spoke.outputs.private_route_table_ids
 
   tags = {
     Phase = "phase-3"
   }
 }
-
