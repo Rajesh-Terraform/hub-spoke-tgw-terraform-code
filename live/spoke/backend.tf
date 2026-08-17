bd6dbeba7ac9b7a2d358e7c70bd41a6f57a307af
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-08-17 20:06:46 +0530

    all terraform code changes

--- live/spoke/backend.tf
+++ live/spoke/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "dhoni-demo-terraform-bucket-123456"
+    bucket       = "harish-gaddam-bucket123"
     key          = "working/spoke/terraform.tfstate"
     region       = "ap-south-1"
     use_lockfile = true
